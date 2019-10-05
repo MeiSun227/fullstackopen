f1432b6da3a16f667db925817c5692f0dc4a8c8d
Mei Sun 227 <meysunlee@gmail.com>
2019-10-05 13:28:43 +0300

    submit part 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 const Header = (props) => {
   return (
     <h1>{props.header}</h1>
-    
+
   )
 }
 
@@ -20,31 +20,55 @@ const App = () => {
   const handleGoodClick = () => {
 
     const newGood = good + 1
-    console.log(good);
     setGood(newGood)
   }
   const handleNeutralClick = () => {
 
     const newNeutral = neutral + 1
-    console.log(neutral);
     setNeutral(newNeutral)
   }
-const handleBadClick = () => {
-  const newBad = bad + 1
-    console.log(bad);
+  const handleBadClick = () => {
+    const newBad = bad + 1
     setBad(newBad)
-}
+  }
+
+  const sum = () => {
+    return good + neutral + bad
+  }
+
+  const averageCalculate = () => {
+    const value = good * 1 + neutral * 0 + bad * -1
+
+    const average = (value / sum()).toFixed(2)
+    if (sum() === 0) {
+      return 0
+    }
+    console.log(average);
+    return average
+  }
+
+  const positiveCalculate = () => {
+    if (sum() === 0) {
+      return 0
+    }
+    return (good / sum() * 100).toFixed(2)
+
+
+  }
   return (
     <div>
       <Header header={header} />
       <button onClick={handleGoodClick}>good</button>
       <button onClick={handleNeutralClick}>neutral</button>
       <button onClick={handleBadClick}>bad</button>
-     
+
       <Header header={statistic} />
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
+      <p>all: {sum()}</p>
+      <p>average : {averageCalculate()}</p>
+      <p>positive : {positiveCalculate()}%</p>
     </div>
   )
 }
