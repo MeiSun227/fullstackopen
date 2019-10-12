dc39bed19b5ba95b0df899210657eba16e193fdc
Mei Sun 227 <meysunlee@gmail.com>
2019-10-12 19:34:44 +0300

    submit part 2.11

--- part2/phonebook/src/app.js
+++ part2/phonebook/src/app.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Person = (props) => {
   return (
@@ -42,19 +43,21 @@ const Filter = (props) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setNewSearch] = useState('')
 
-
-  const addPerson = (event) => {
+  useEffect(() => { 
+    axios
+    .get('http://localhost:3001/persons') 
+    .then(response => {
+      setPersons(response.data)
+    })
+    },[])
+ 
+ 
+    const addPerson = (event) => {
     event.preventDefault()
     const personsObject = { name: newName, number: newNumber }
     const names = persons.map((person) => person.name)
