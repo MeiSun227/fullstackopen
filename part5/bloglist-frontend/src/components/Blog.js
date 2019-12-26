ca02a226c3d6b6687be656b19790ccc40da03b6e
Mei Sun 227 <meysunlee@gmail.com>
2019-12-26 19:33:20 +0200

    submit part 5.10

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
 const Blog = ({ blog,handleLikeChange,handleDeleteBlog}) => {
-
+  const savedUser = JSON.parse(window.localStorage.getItem('loggedBlogappUser'))
+if (blog.user.username === savedUser.username) {
   return (
     <>
       <p> <a href="#">{blog.url}</a></p>
@@ -15,5 +16,18 @@ const Blog = ({ blog,handleLikeChange,handleDeleteBlog}) => {
       </div>
     </>
   )
+} else{
+  return (
+    <>
+      <p> <a href="#">{blog.url}</a></p>
+      <div>
+        {blog.likes} likes
+  <button onClick={(event) => handleLikeChange(blog, event)}>like</button>
+      </div>
+      <p>added by {blog.author}</p>
+    </>
+  )
+}
+
 }
 export default Blog
